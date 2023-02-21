fa60606b0ce457b7d5b29ea7f0505e14304efb1d
Sayali Ghadge <sayali13579@gmail.com>
2023-02-20 17:27:40 -0800

    initial commit

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+#if UNITY_IOS
+        string dir = "AssetBundles/iOS";
+        if (Directory.Exists(dir) == false)
+        {
+            Directory.CreateDirectory(dir);
+        }
+        //BuildTarget 选择build出来的AB包要使用的平台
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.iOS);
+#else       
+        string dir = "AssetBundles/Android";
+        if (Directory.Exists(dir) == false)
+        {
+            Directory.CreateDirectory(dir);
+        }
+        //BuildTarget 选择build出来的AB包要使用的平台
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.Android);
+#endif
+    }
+}
